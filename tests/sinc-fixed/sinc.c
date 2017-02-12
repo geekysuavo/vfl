2e7f3eca862fca9711b3b66bc5bfff167bd17bc9
Bradley Worley <geekysuavo@gmail.com>
2017-02-12 17:09:19 +0100

    Added two additional regression tests
    This commit adds the 'sinc' and 'sinc-fixed' tests, which
    demonstrate the 'impulse' and 'fixed-impulse' factors,
    respectively.

---
+++ tests/sinc-fixed/sinc.c
@@ -0,0 +1,77 @@
+
+/* include the required headers. */
+#include <vfl/optim.h>
+
+/* main(): application entry point.
+ *
+ * arguments:
+ *  @argc: number of command-line argument strings.
+ *  @argv: array of command-line argument strings.
+ *
+ * returns:
+ *  application execution return code.
+ */
+int main (int argc, char **argv) {
+  /* silly hack to avoid warnings. */
+  if (!argc || !argv)
+    return 1;
+
+  /* read the dataset file. */
+  data_t *dat = data_alloc();
+  data_fread(dat, "sinc.dat");
+
+  /* set up a regression model. */
+  model_t *mdl = model_vfr(1000.0, 10.0, 1.0e-3);
+  model_set_data(mdl, dat);
+
+  /* add factors to the model. */
+  for (unsigned int i = 0; i < dat->N; i++) {
+    const double xi = matrix_get(dat->X, i, 0);
+    factor_t *f = factor_fixed_impulse(xi, 0.001);
+    model_add_factor(mdl, f);
+  }
+
+  /* initialize the factor precisions. */
+  for (unsigned int j = 0; j < mdl->M; j++)
+    factor_set(mdl->factors[j], 0, 1.0);
+
+  /* set up an optimizer. */
+  optim_t *opt = optim_fg(mdl);
+  opt->l0 = 0.001;
+  opt->dl = 0.1;
+  unsigned int iter;
+  double bound;
+
+  /* perform a few optimization iterations. */
+  for (iter = 0; iter < 1000; iter++) {
+    bound = model_bound(mdl);
+    fprintf(stderr, "%u %le", iter, bound);
+    int mod = opt->iterate(opt);
+    fprintf(stderr, "\n");
+    if (!mod) break;
+  }
+
+  /* allocate datasets for prediction. */
+  double grid_values[] = { -10.0, 0.001, 10.0 };
+  matrix_view_t grid = matrix_view_array(grid_values, 1, 3);
+  data_t *mean = data_alloc_from_grid(&grid);
+  data_t *var = data_alloc_from_grid(&grid);
+
+  /* compute the prediction. */
+  model_predict_all(mdl, mean, var);
+
+  /* output the prediction. */
+  data_fwrite(mean, "mean.dat");
+  data_fwrite(var, "var.dat");
+
+  /* free the structures. */
+  optim_free(opt);
+  model_free(mdl);
+  data_free(mean);
+  data_free(var);
+  data_free(dat);
+
+  /* return success. */
+  return 0;
+}
+
