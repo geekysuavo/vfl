8f5d52be539f3f87855eedc55256f992f66cc13a
Bradley Worley <geekysuavo@gmail.com>
2017-02-18 11:32:47 +0100

    Added a multi-exponential fitting test
    This commit adds a test of multi-exponential fitting using the
    exponential decay factor.

--- tests/multexp/multexp.c
+++ tests/multexp/multexp.c
@@ -18,20 +18,22 @@ int main (int argc, char **argv) {
 
   /* read the dataset file. */
   data_t *dat = data_alloc();
-  data_fread(dat, "gauss.dat");
+  data_fread(dat, "multexp.dat");
 
   /* set up a regression model. */
-  model_t *mdl = model_vfr(100.0, 100.0, 1.0e-2);
+  model_t *mdl = model_vfr(10.0, 40.0, 1.0e-3);
   model_set_data(mdl, dat);
 
   /* add factors to the model. */
+  model_add_factor(mdl, factor_decay(10.0, 1.0));
+  model_add_factor(mdl, factor_decay(10.0, 10.0));
+  model_add_factor(mdl, factor_decay(10.0, 100.0));
   model_add_factor(mdl, factor_decay(10.0, 1000.0));
-  model_add_factor(mdl, factor_impulse(60.0, 0.01));
-  model_add_factor(mdl, factor_impulse(180.0, 0.01));
+  model_add_factor(mdl, factor_decay(10.0, 10000.0));
   
   /* set up an optimizer. */
   optim_t *opt = optim_fg(mdl);
-  opt->l0 = 1.0;
+  opt->l0 = 0.001;
   opt->dl = 0.1;
   unsigned int iter;
   double bound;
@@ -46,7 +48,7 @@ int main (int argc, char **argv) {
   }
 
   /* allocate datasets for prediction. */
-  double grid_values[] = { 0.0, 1.0, 300.0 };
+  double grid_values[] = { 0.0, 0.1, 150.0 };
   matrix_view_t grid = matrix_view_array(grid_values, 1, 3);
   data_t *mean = data_alloc_from_grid(&grid);
   data_t *var = data_alloc_from_grid(&grid);
